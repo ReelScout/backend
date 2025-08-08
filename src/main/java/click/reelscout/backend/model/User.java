ee147107cb94d8ffc21a32eda74411543e7d5e6f
matteodefinis <matteopiodefinis@gmail.com>
2025-08-08 12:21:35 +0200

    Refactor: Update NoArgsConstructor access level to PROTECTED in User abstract entity

--- src/main/java/click/reelscout/backend/model/User.java
+++ src/main/java/click/reelscout/backend/model/User.java
@@ -10,7 +10,7 @@ import java.util.Collection;
 import java.util.List;
 
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Inheritance(strategy = InheritanceType.JOINED)
 @Entity
 @Getter
