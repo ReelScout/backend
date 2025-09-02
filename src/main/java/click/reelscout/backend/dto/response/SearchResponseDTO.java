108c8f9f2e6154de5bfdaa8d8f77be8f08f8200e
matteodefinis <matteopiodefinis@gmail.com>
2025-09-02 21:57:44 +0200

    Refactor: Remove JsonInclude annotation from SearchResponseDTO as it breaks compatibility with frontend

--- src/main/java/click/reelscout/backend/dto/response/SearchResponseDTO.java
+++ src/main/java/click/reelscout/backend/dto/response/SearchResponseDTO.java
@@ -1,6 +1,5 @@
 package click.reelscout.backend.dto.response;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -8,7 +7,6 @@ import java.util.List;
 
 @AllArgsConstructor
 @Data
-@JsonInclude(JsonInclude.Include.NON_EMPTY)
 public class SearchResponseDTO<S extends  UserResponseDTO> {
     private List<S> users;
 
