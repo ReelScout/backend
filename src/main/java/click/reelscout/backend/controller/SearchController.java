e589f2672d9b4cd949b84af867ae3bc0e81700a5
matteodefinis <matteopiodefinis@gmail.com>
2025-09-08 21:50:17 +0200

    Feature: Add member search functionality to SearchController and enhance SearchService with member-specific query support

--- src/main/java/click/reelscout/backend/controller/SearchController.java
+++ src/main/java/click/reelscout/backend/controller/SearchController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RequiredArgsConstructor
 @Cacheable
 @RequestMapping("${api.paths.search}")
@@ -21,5 +23,10 @@ public class SearchController<S extends UserResponseDTO> {
     @GetMapping
     public ResponseEntity<SearchResponseDTO<S>> search(@RequestParam String query) {
         return ResponseEntity.ok(searchService.search(query));
+    }
+
+    @GetMapping("/members")
+    public ResponseEntity<List<S>> searchMembers(@RequestParam String query) {
+        return ResponseEntity.ok(searchService.searchMembers(query));
     }
 }
